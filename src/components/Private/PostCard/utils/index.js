724520cc8c454f08afd0433ccc367952beff7afc
Sherzod Tohirov <sherzod@Yaroslavs-iMac.local>
2024-05-14 12:28:13 +0500

    Something

--- src/components/Private/PostCard/utils/index.js
+++ src/components/Private/PostCard/utils/index.js
@@ -1,22 +1,27 @@
+
 export function refineTimestamp(timestamp) {
     const date = new Date();
-    const diff = Number(date.getTime()) - Number(timestamp);
+    let diff = Number(date.getTime()) - Number(timestamp);
     let hours = 0;
     let minutes = 0;
+    let days = 0;
 
     if(diff > 3600000) {
-        hours = parseInt(diff - diff / 3600000)
-        minutes = parseInt((diff - diff / 3600000) / 60000)
-        
+        hours = parseInt(diff / 360000)
+        minutes = parseInt((Number(diff % 3600000)) / 60000)
+
     }else {
         if(diff > 60000) {
             minutes = parseInt((diff - diff / 3600000) / 60000)
         }
     }
 
-    console.log(diff)
-    if(hours && minutes) {
-         return `${hours}h ${minutes}m ago`
+    if(hours > 24) {
+        days = parseInt(hours / 24);
+        hours = parseInt(hours % 24);
+    }
+    if(hours && minutes && days ) {
+         return `${days} days ${hours}h ${minutes}m ago`
     }else if(minutes) {
         return `${minutes}m ago`
     }else {
